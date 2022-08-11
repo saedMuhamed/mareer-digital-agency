216165f99a2459bdd414a97a7ff1a5bb9bb2d00b
Abdifatah Muse <35341044+abdifatahmuse@users.noreply.github.com>
2022-08-12 00:07:27 +0300

    Router remove the div

--- src/Router.js
+++ src/Router.js
@@ -7,16 +7,14 @@ import "./App.css";
 const Router = () => {
   return (
     <BrowserRouter>
-      <div className=" bg-white dark:bg-primary-dark transition duration-300">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/ourTeam" element={<OurTeam />} />
-          <Route path="/contactus" element={<Contactus />} />
-          <Route path="/mapSection" element={<MapSection />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/ourTeam" element={<OurTeam />} />
+        <Route path="/contactus" element={<Contactus />} />
+        <Route path="/mapSection" element={<MapSection />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </BrowserRouter>
   );
 };
